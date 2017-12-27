73d7184e7e379fdf819fca7252276c3453f2110a
Constantine19 <czelmanovich@gmail.com>
2017-12-27 01:49:55 -0500

    ttt

---
+++ Day6-Let's Review.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+for j in range (0,T):
+    even, odd = [],[]
+    S = raw_input()
+    i = 0
+    for letter in S:
+        
+        if (i%2 != 0):
+            odd.append(letter)
+        else:
+            even.append(letter)
+        i += 1
+    print "".join(even) + " " + "".join(odd)
